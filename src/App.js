db053487838a6ab64a220220bcbe96ddb33a75d3
erk kobayashi <kouyoumikan136@gmail.com>
2022-04-11 17:33:41 +0900

    Todoリスト Form機能の追加

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container, Form, Input, InputGroup } from 'reactstrap';
+import { Container, Form, Input, InputGroup, InputGroupAddon, Button } from 'reactstrap';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addTodo(value)
+    console.log(value)
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, text]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
   return (
     <div className="App">
       <Container>
         <h1 className="mt-4">Todo リスト</h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputGroup>
-            <Input text="text" value=""></Input>
+            <Input text="text"
+             value={value}
+             onChange={e => setValue(e.target.value)}></Input>
             <InputGroupAddon type="append">
               <Button type="submit" color="primary">追加</Button>
             </InputGroupAddon>
