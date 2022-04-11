83da762808a9d2d63338b63df7dd8e308edd38ec
erk kobayashi <kouyoumikan136@gmail.com>
2022-04-11 17:20:57 +0900

    Todoリスト 見た目の追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Form, Input, InputGroup } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form>
+          <InputGroup>
+            <Input text="text" value=""></Input>
+            <InputGroupAddon type="append">
+              <Button type="submit" color="primary">追加</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
     </div>
   );
 }
